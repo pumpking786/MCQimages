1a7b5be20822fca4a32c151d52d4fcb565985d57
Pramit Amatya <130182347+pumpking786@users.noreply.github.com>
2025-04-29 16:11:17 +0545

    Add files via upload

---
+++ backend/migrations/20250419075454-create-user.js
@@ -0,0 +1,54 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      age: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.ENUM("user", "admin"),
+        allowNull: false,
+        defaultValue: "user",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal(
+          "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"
+        ),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Users");
+    await queryInterface.sequelize.query('DROP TYPE IF EXISTS "enum_Users_role";');
+  },
+};
---
+++ backend/migrations/20250429071458-create-questions.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("questions", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      questionImage: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      question: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      options: {
+        type: Sequelize.JSON, // Compatible with MySQL
+        allowNull: false,
+      },
+      correctAnswer: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      hint: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("questions");
+  }
+};
