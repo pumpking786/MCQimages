98f4a7080f39060b2bda456d313be0d75f9251ff
Pramit Amatya <130182347+pumpking786@users.noreply.github.com>
2025-05-21 11:08:44 +0545

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -8,8 +8,5 @@ router.post("/signup", AuthController.signup);
 router.post("/login", AuthController.login);
 router.get("/check-session", AuthController.checkSession);
 router.post("/logout", AuthController.logout);
-router.get("/user-details", authenticateJWT, AuthController.getUserDetails);
-router.put("/user-details", authenticateJWT, AuthController.updateUserDetails);
-router.delete("/user-details", authenticateJWT, AuthController.deleteUser);
 
 module.exports = router;
